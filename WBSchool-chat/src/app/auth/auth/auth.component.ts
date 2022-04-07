8663d0d4cb1a2573257a1ec212dbc954872c9818
Alexander <warrior0edem@gmail.com>
2022-04-07 17:12:30 +0300

    added buttons

--- WBSchool-chat/src/app/auth/auth/auth.component.ts
+++ WBSchool-chat/src/app/auth/auth/auth.component.ts
@@ -3,8 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styles: [
-  ]
+  styles: [`
+  .nav button {
+    margin: 10px;
+  }
+  `]
 })
 export class AuthComponent implements OnInit {
 
