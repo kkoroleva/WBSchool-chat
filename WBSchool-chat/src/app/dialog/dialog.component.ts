8c2097a422b0a4fc52f3436423f6c31fa2aa076a
pasha <pashaganiev20@gmail.com>
2022-04-08 19:08:57 +0300

    add-version

--- WBSchool-chat/src/app/dialog/dialog.component.ts
+++ WBSchool-chat/src/app/dialog/dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IMessage } from './dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,12 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
+  message = new FormControl('');
+
+  myId:number = 264; 
+
+  messageData:IMessage[] =[
+
+    {
+    text: "Hey There !",
+    owner: 263,
+    date:'Today, 2:01pm'
+    },
+    {
+    text: "How are you doing? ",
+    owner: 263,
+    date:'Today, 2:02pm'
+    },
+    {
+    text: "Hello...",
+    owner: 264,
+    date:'Today, 2:12pm'
+    },
+    {
+    text: "I am good  and hoew about you?",
+    owner: 264,
+    date:'Today, 2:12pm'
+    },
+    {
+    text: "I am doing well. Can we meet up tomorrow?",
+    owner: 263,
+    date:'Today, 2:12pm'
+    },
+    {
+    text: "Sure!",
+    owner: 264,
+    date:'Today, 2:14pm'
+    }
+
+]
+
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  callPhone(){
-    console.log("call")
+  sendMessage(event:KeyboardEvent) {
+    if (this.message.value.trim() && event.key === 'Enter') {
+      this.messageData.push({
+        text: this.message.value,
+        owner: this.myId,
+        date: 'Today, 7:12pm',
+      });
+
+      this.message.setValue('');
+    }
   }
+ 
 }
