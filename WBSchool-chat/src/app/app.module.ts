016561d63769ed8538fd3fb5ed2e617d356ea26c
“Nikita <nikitaberezhnov27@gmail.com>
2022-04-08 12:09:47 +0300

    add navbar

--- WBSchool-chat/src/app/app.module.ts
+++ WBSchool-chat/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
 /*Components */
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 
 /*Modules*/
@@ -21,18 +21,23 @@ import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTabsModule} from '@angular/material/tabs';
-import {MatChipsModule, } from '@angular/material/chips';
+import {MatChipsModule,} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
+import {NavbarComponent} from './navbar/navbar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+
 
     //Material UI
     BrowserAnimationsModule,
@@ -45,6 +50,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatTabsModule,
     MatChipsModule,
     MatIconModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
 
     //Forms
     FormsModule,
@@ -53,4 +60,5 @@ import {MatIconModule} from '@angular/material/icon';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
