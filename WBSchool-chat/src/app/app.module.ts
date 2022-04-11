2a9557facc72287d1f450474299523d353b352fd
Alexander <warrior0edem@gmail.com>
2022-04-11 16:40:29 +0300

    fix bugs 2

--- WBSchool-chat/src/app/app.module.ts
+++ WBSchool-chat/src/app/app.module.ts
@@ -31,10 +31,6 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatChipsModule } from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NotificationsPageComponent } from './notifications-page/notifications-page.component';
-import { MessagesPageComponent } from './messages-page/messages-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 import { AuthModule } from './auth/auth.module';
