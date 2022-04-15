ce695a20679a8c825a925be3873ff87fcb2f7228
kkoroleva <korolevakarinka22@gmail.com>
2022-04-15 12:43:46 +0300

    style fixes

--- WBSchool-chat/src/app/groups/groups.service.ts
+++ WBSchool-chat/src/app/groups/groups.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IGroup } from './group';
 import { IFriend } from '../friends/friend';
+import { IUnread } from '../unread/unread';
 
 @Injectable({
   providedIn: 'root',
@@ -17,10 +18,22 @@ export class GroupsService {
   }
 
   getPrivateChats(): Observable<IFriend[]> {
-    return this.http.get<IFriend[]>(`${this.apiUrl}/chats`);
+    return this.http.get<IFriend[]>(`${this.apiUrl}/chats/friends`);
+  }
+
+  getAllChats(): Observable<IUnread[]> {
+    return this.http.get<IUnread[]>(`${this.apiUrl}/chats`);
   }
 
   createGroupChat(group: IGroup): Observable<IGroup> {
     return this.http.post<IGroup>(`${this.apiUrl}/chats`, group);
+  }
+
+  createPrivateChat(chat: IFriend): Observable<IFriend> {
+    return this.http.post<IFriend>(`${this.apiUrl}/chats`, chat);
+  }
+
+  markEveryChatUnread(chats: IUnread[]): Observable<IUnread> {
+    return this.http.patch<IUnread>(`${this.apiUrl}/chats`, chats);
   }
 }
