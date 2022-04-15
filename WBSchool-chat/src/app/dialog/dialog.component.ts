71e0a0c908714c2e9a3921824c133c65d2a173cd
Alexander <warrior0edem@gmail.com>
2022-04-15 10:09:13 +0300

    added images

--- WBSchool-chat/src/app/dialog/dialog.component.ts
+++ WBSchool-chat/src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { IMessage } from './dialog';
@@ -9,8 +9,7 @@ import { DialogService } from './dialog.service';
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent implements OnInit, AfterViewChecked {
-  @ViewChild("wrapper") wrapper!:ElementRef
+export class DialogComponent implements OnInit {
 
   editMessageID:string = '';
 
@@ -36,10 +35,6 @@ export class DialogComponent implements OnInit, AfterViewChecked {
     this.getMessages()
   };
 
-  ngAfterViewChecked(): void {
-    this.changeScroll();
-  }
-
   addImage(input: any) {
     console.log(this.formData.imageOrFile)
     let imageOrFile: string | ArrayBuffer| null | any = '';
@@ -52,7 +47,7 @@ export class DialogComponent implements OnInit, AfterViewChecked {
     reader.readAsDataURL(file);
   }
   
-  getMessages():void {///==============для получение сообщение 
+  getMessages():void {
     this.service.getMessages().subscribe((res)=> {
       // (res.includes(".png") || this.message.value.includes(".jpeg") || this.message.value.includes(".jpg") || this.message.value.includes(".svg"))
       // res.forEach(elem => {
@@ -69,7 +64,6 @@ deleteMessage(id:string):void {
     }
   )
 };
-
 deleteChat(){
   console.log('удалить чат')
 }
@@ -83,32 +77,19 @@ editMessage(text:string, id:string):void {
   )
 };
 
-getMessage(id:string, text:string):void {
+getMessage(id:string, text:string): void {///================для получение сообщение в инпуте привязен к кнопке изменить 
   this.isEditMessage = true;
   this.editMessageID = id;
   this.message.setValue(text);
   this.image = btoa(this.formData.image);
 };
 
-changeScroll(){
-  this.wrapper.nativeElement.scrollTop = this.wrapper.nativeElement.scrollHeight;
-}
-
 sendMessage(event:KeyboardEvent):void {
     if (this.message.value.trim() && event.key === 'Enter' || this.formData.image) {
-
-
 
       if(this.isEditMessage){
         this.editMessage(this.message.value, this.editMessageID)
       }
-      // else if (this.message.value.includes(".png") || this.message.value.includes(".jpeg") || this.message.value.includes(".jpg") || this.message.value.includes(".svg")){
-      //   this.service.sendMessage(btoa(this.message.value), this.formData.imageOrFile).subscribe(
-      //     () => {
-      //       this.getMessages()
-      //     }
-      //   )
-      // }
       else {
         this.service.sendMessage(this.message.value, this.formData.imageOrFile).subscribe(
           () => {
