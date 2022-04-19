924a93089cfc2e568db55c79428fda5a3146d2e0
kkoroleva <korolevakarinka22@gmail.com>
2022-04-19 14:06:04 +0300

    hot fix

--- WBSchool-chat/src/app/app-routing.module.ts
+++ WBSchool-chat/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: 'chat', component: MessagesPageComponent, canActivate: [AuthGuardService]},
   { path: 'alerts', component: NotificationsPageComponent, canActivate: [AuthGuardService]},
   { path: 'profile', component: ProfilePageComponent, canActivate: [AuthGuardService]},
-  { path: '**', component: NotFoundPageComponent, canActivate: [AuthGuardService]},
+  { path: '**', component: NotFoundPageComponent},
 ];
 
 @NgModule({
