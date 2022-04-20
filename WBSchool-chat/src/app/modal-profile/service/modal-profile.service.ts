275fd47df3f17723b2115833181fa686e92dee5c
Leonid (notebook) <leonidrawhide@gmail.com>
2022-04-20 20:23:51 +0300

    made commit as git asked

---
+++ WBSchool-chat/src/app/modal-profile/service/modal-profile.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalProfileComponent } from '../modal-profile.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalProfileService {
+
+  constructor(
+    private modalProfileServ: ModalProfileService, 
+    public dialog: MatDialog
+  ) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalProfileComponent, {
+      panelClass: 'modal-profile'
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The dialog was closed');
+    });
+  }
+}
