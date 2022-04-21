98010870871873f3b2f2fc07e20ca7e767ae4a5b
byteCD <dima.s322@yandex.ru>
2022-04-21 20:46:56 +0600

    Update private chats modal

--- WBSchool-chat/src/app/friends/create-private-chat/create-private-chat.component.ts
+++ WBSchool-chat/src/app/friends/create-private-chat/create-private-chat.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Actions, ofType } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
-import { map, Observable } from 'rxjs';
-import { INewUser, IUserData } from 'src/app/auth/interfaces';
-import { IUser } from 'src/app/groups/user';
+import { map, Observable, startWith } from 'rxjs';
+import { IUserData } from 'src/app/auth/interfaces';
 import {
   initContacts,
   pushContacts,
@@ -15,9 +14,7 @@ import {
   pushToFriends,
 } from 'src/app/store/actions/groups.actions';
 import { IGroupsState } from 'src/app/store/reducers/groups.reducers';
-import { selectUser } from 'src/app/store/selectors/auth.selectors';
 import { selectContacts } from 'src/app/store/selectors/contacts.selectors';
-import { IFriend } from '../friend';
 
 @Component({
   selector: 'app-create-private-chat',
@@ -25,16 +22,13 @@ import { IFriend } from '../friend';
   styleUrls: ['./create-private-chat.component.scss'],
 })
 export class CreatePrivateChatComponent implements OnInit {
-  public userState$: Observable<IUserData> = this.store$.pipe(
-    select(selectUser)
-  );
-  public contacts$: Observable<IUser[]> = this.store$.pipe(
+  public contactsList: IUserData[] = [];
+  public contactsControl!: FormControl;
+  public form: FormGroup;
+  public contacts$: Observable<IUserData[]> = this.store$.pipe(
     select(selectContacts),
     map((contacts) => contacts.contacts)
   );
-  public contactsList: IUser[] = [];
-  public contactsControl!: FormControl;
-  public form: FormGroup;
 
   constructor(
     private dialogRef: MatDialogRef<CreatePrivateChatComponent>,
@@ -62,22 +56,28 @@ export class CreatePrivateChatComponent implements OnInit {
     this.actions$.pipe(ofType(pushContacts)).subscribe(({ contacts }) => {
       this.contactsList = contacts.contacts;
 
-      // this.contacts$ = this.contactsControl.valueChanges.pipe(
-      //   startWith(''),
-      //   map((username: string | null) =>
-      //     username ? this.filterContacts(username) : this.contactsList
-      //   )
-      // );
+      this.contacts$ = this.contactsControl.valueChanges.pipe(
+        startWith(''),
+        map((username: string | null) =>
+          username ? this.filterContacts(username) : this.contactsList
+        )
+      );
     });
   }
 
   createPrivateChat(): void {
-    const contacts: IUser = this.contactsControl.value;
-    const username: string = contacts.username;
-    const friend: IFriend = this.contactsControl.value;
+    const username: string = this.contactsControl.value;
 
     if (this.form.valid) {
-      this.store$.dispatch(createChatFriend({ friend, username }));
+      this.store$.dispatch(createChatFriend({ username }));
     }
+  }
+
+  private filterContacts(username: string): IUserData[] {
+    const filterUsername = username.toLowerCase();
+
+    return this.contactsList.filter((contact) =>
+      contact.username.toLowerCase().includes(filterUsername)
+    );
   }
 }
