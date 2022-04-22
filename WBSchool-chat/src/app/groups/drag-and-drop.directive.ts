18599b9ecd2adde78343d620058d371e51602524
byteCD <dima.s322@yandex.ru>
2022-04-22 18:12:10 +0600

    Update drag and drop

--- WBSchool-chat/src/app/groups/drag-and-drop.directive.ts
+++ WBSchool-chat/src/app/groups/drag-and-drop.directive.ts
@@ -1,16 +1,36 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDragAndDrop]',
 })
-export class DragAndDropDirective {
-  constructor(private el: ElementRef) {}
+export class DragAndDropDirective implements AfterViewInit {
+  private text!: ElementRef;
 
   @HostListener('dragover') dragOver(): void {
-    this.el.nativeElement.style.borderColor = 'rgb(20, 126, 64)';
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      'border-color',
+      'rgb(20, 126, 64)'
+    );
+
+    this.renderer.setStyle(this.text, 'color', 'rgb(20, 126, 64)');
   }
 
   @HostListener('dragleave') dragEnd(): void {
-    this.el.nativeElement.style.borderColor = '#000';
+    this.renderer.setStyle(this.el.nativeElement, 'border-color', '#000');
+
+    this.renderer.setStyle(this.text, 'color', '#000');
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.text = this.el.nativeElement.querySelector('.modal__drag-text');
   }
 }
