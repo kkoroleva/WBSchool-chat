b05818380609ee0732063e19b6529fac763a33c7
byteCD <dima.s322@yandex.ru>
2022-04-27 22:01:20 +0600

    Fix errors

--- WBSchool-chat/src/app/dialog/dialog.component.ts
+++ WBSchool-chat/src/app/dialog/dialog.component.ts
@@ -1,8 +1,18 @@
+import { selectChatGroup } from './../store/selectors/groups.selectors';
 import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IGroupsState } from '../store/reducers/groups.reducers';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
 })
-export class DialogComponent {}
+export class DialogComponent {
+  public chatGroup$: Observable<string> = this.store$.pipe(
+    select(selectChatGroup)
+  );
+
+  constructor(private store$: Store<IGroupsState>) {}
+}
