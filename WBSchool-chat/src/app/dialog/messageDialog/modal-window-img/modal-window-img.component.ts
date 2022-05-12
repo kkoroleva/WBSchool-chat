f33818a549ef3f24484fffc7c8820486b8d1a49a
DaoNik <nikitataranin@gmail.com>
2022-05-12 13:00:12 +0300

    fix open image

--- WBSchool-chat/src/app/dialog/messageDialog/modal-window-img/modal-window-img.component.ts
+++ WBSchool-chat/src/app/dialog/messageDialog/modal-window-img/modal-window-img.component.ts
@@ -4,20 +4,17 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-modal-window-img',
   templateUrl: './modal-window-img.component.html',
-  styleUrls: ['./modal-window-img.component.scss']
+  styleUrls: ['./modal-window-img.component.scss'],
 })
 export class ModalWindowImgComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ModalWindowImgComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
 
-  constructor(public dialogRef: MatDialogRef<ModalWindowImgComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string) { }
-
-  ngOnInit(): void {
-  }
-
- 
+  ngOnInit(): void {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
